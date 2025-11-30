4ef3de631b606b35924a43f6edf78b6f4a9fe2c9
Eren Urkac <erenurkac28@gmail.com>
2025-11-30 15:02:12 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number1, number2;
+            char operation;
+            int result;
+            Console.Write("Enter first number:");
+          number1=  int.Parse(Console.ReadLine());
+            Console.Write("Enter second number"); 
+            number2= int.Parse(Console.ReadLine());
+            Console.Write("Choose the operator +,-,*,/");
+            operation = Console.ReadLine();
+            switch (operation)
+            {
+                case '+':result = number1 + number2;
+                 
+                      Console.WriteLine(result);
+                    break;
+                case '-':
+                    result = number1 - number2;
+
+                    Console.WriteLine(result);
+                    break;
+                case '*':
+                    result = number1 * number2;
+
+                    Console.WriteLine(result);
+                    break;
+                case '/':
+                    result = number1 / number2;
+
+                    Console.WriteLine(result);
+                    break;
+            }
+        }
+    }
+}
